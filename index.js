7bf04906ed6c635d1a8f48d479d6bcfbf6c98179
Patit <plie0002@student.monash.edu>
2023-09-15 00:23:55 +1000

    add post exercise

--- index.js
+++ index.js
@@ -17,8 +17,15 @@ mongoose.connect("mongodb+srv://MongoDB_Backend:voGoFuB_9122110@cluster0.q5x6eti
 const userSchema = new Schema({
   username: String
 });
+const exerciseSchema = new Schema({
+  username: String,
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: String, required: true },
+});
 
 const User = mongoose.model("User", userSchema);
+const Exercise = mongoose.model("Exercise", exerciseSchema);
 
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -42,4 +49,34 @@ app.get('/api/users', async function(req, res) {
   console.log("hereeeeeeeeeeee");
   console.log(typeof userList);
   res.send(userList);
+});
+
+app.post('/api/users/:_id/exercises', async function(req, res) {
+  const inputId = req.body[":_id"];
+  console.log(req["body"][":_id"]);
+
+  const inputDescription = req.body.description;
+  const inputDuration = req.body.duration;
+  const date = req.body.date;
+  const inputUser = await User.findOne({_id: inputId});
+  console.log(inputUser);
+  const inputUsername = inputUser.username;
+  var inputDate;
+
+  if (date === undefined || date === ""){
+    inputDate = new Date();
+  } else {
+    inputDate = date;
+  }
+
+  const check = await Exercise.find({_id: inputId});
+  const exercise = new Exercise({username: inputUsername, _id: inputId, description: inputDescription, duration: inputDuration, date: inputDate});
+
+  if(check === null) {
+    await exercise.save();
+  } else {
+    await Exercise.findOneAndUpdate({_id: inputId}, {description: inputDescription, duration: inputDuration, date: inputDate}, {new: true});
+  }
+
+  res.json(exercise);
 });
