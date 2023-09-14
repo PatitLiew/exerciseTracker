f7dfc95ff24e28fa47a7b3b28d2ddb90c58531e9
Patit <plie0002@student.monash.edu>
2023-09-14 17:39:36 +1000

    -add post/get for user

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+var bodyParser = require("body-parser");
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,10 +12,34 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+mongoose.connect("mongodb+srv://MongoDB_Backend:voGoFuB_9122110@cluster0.q5x6eti.mongodb.net/", { useNewUrlParser: true, useUnifiedTopology: true });
 
+const userSchema = new Schema({
+  username: String
+});
 
+const User = mongoose.model("User", userSchema);
 
+app.use(bodyParser.urlencoded({extended: false}));
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+
+app.post('/api/users', async function(req, res) {
+  const userUsername = req.body.username;
+
+    // Create a new document and save it to the database
+    const user = new User({username: userUsername});
+    await user.save();
+
+    res.json({username: user.username, _id: user._id});
+});
+
+app.get('/api/users', async function(req, res) {
+  const userList = await User.find({});
+  console.log("hereeeeeeeeeeee");
+  console.log(typeof userList);
+  res.send(userList);
+});
