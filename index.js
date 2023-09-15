78e7a72780aa5b4bcf3db8c684853292b0869add
Patit <plie0002@student.monash.edu>
2023-09-15 10:35:04 +1000

    complete

--- index.js
+++ index.js
@@ -17,15 +17,21 @@ mongoose.connect("mongodb+srv://MongoDB_Backend:voGoFuB_9122110@cluster0.q5x6eti
 const userSchema = new Schema({
   username: String
 });
-const exerciseSchema = new Schema({
-  username: String,
-  description: { type: String, required: true },
-  duration: { type: Number, required: true },
-  date: { type: String, required: true },
-});
-
 const User = mongoose.model("User", userSchema);
+
+const exerciseSchema = new Schema({
+  description: { type: String, required: true, default: 'empty'},
+  duration: { type: Number, required: true ,default: 10},
+  date: { type: String, required: true ,default: 'empty'},
+  }, { _id: false });
 const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+const logsListSchema = new Schema({
+  username: String,
+  count: { type: Number, required: true ,default: 0},
+  log: {type: [exerciseSchema], required: true ,default: []}
+}, {versionKey: false });
+const LogsList = mongoose.model("LogsList", logsListSchema);
 
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -38,8 +44,13 @@ app.post('/api/users', async function(req, res) {
   const userUsername = req.body.username;
 
     // Create a new document and save it to the database
+    console.log("error hereeeeeeeeeeee");
     const user = new User({username: userUsername});
+    const logsList = new LogsList({username: user.username, _id: user._id})
+      console.log(user);
+        console.log(typeof user);
     await user.save();
+    await logsList.save();
 
     res.json({username: user.username, _id: user._id});
 });
@@ -53,30 +64,40 @@ app.get('/api/users', async function(req, res) {
 
 app.post('/api/users/:_id/exercises', async function(req, res) {
   const inputId = req.body[":_id"];
-  console.log(req["body"][":_id"]);
 
   const inputDescription = req.body.description;
-  const inputDuration = req.body.duration;
+  const inputDuration = parseInt(req.body.duration);
   const date = req.body.date;
-  const inputUser = await User.findOne({_id: inputId});
-  console.log(inputUser);
-  const inputUsername = inputUser.username;
   var inputDate;
+  const inputUser = await User.findOne({_id: inputId});
+  const inputUsername = inputUser.username;
 
   if (date === undefined || date === ""){
-    inputDate = new Date();
+    inputDate = new Date(); // Create a new Date object with the current date and time in the local time zone
   } else {
-    inputDate = date;
+    inputDate = new Date(date);
   }
+  inputDate = inputDate.toDateString();
 
-  const check = await Exercise.find({_id: inputId});
-  const exercise = new Exercise({username: inputUsername, _id: inputId, description: inputDescription, duration: inputDuration, date: inputDate});
+  const exercise = new Exercise({description: inputDescription, duration: inputDuration, date: inputDate});
 
-  if(check === null) {
-    await exercise.save();
-  } else {
-    await Exercise.findOneAndUpdate({_id: inputId}, {description: inputDescription, duration: inputDuration, date: inputDate}, {new: true});
+  const logsList = await LogsList.find({_id: inputId});
+
+  if(logsList === null) {
+    logsList = new LogsList({username: "empty"});
   }
+  logsList[0].count += 1;
+  logsList[0].log.push(exercise);
+  await logsList[0].save();
 
-  res.json(exercise);
+  res.json({_id: logsList[0]._id, username: logsList[0].username, date: inputDate, duration: inputDuration, description: inputDescription});
+});
+
+app.get('/api/users/:_id/logs', async function(req, res) {
+
+  const inputId = req.params._id;
+  console.log(req.params._id);
+
+    const logsList = await LogsList.findOne({ _id: inputId });
+    res.json(logsList);
 });
